1a2e5c88a3df3a75b38545f0809a93027b3adada
HafizA <121396190+aiscream23@users.noreply.github.com>
2024-02-16 15:31:42 +0800

    Add files via upload

---
+++ Retrieve_UAS_data.py
@@ -0,0 +1,108 @@
+import time
+import calendar
+import os
+import re
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox()
+
+year = 2023
+# month = 3
+
+for month in range(1,13):
+    try:
+        url = f"https://weather.uwyo.edu/cgi-bin/sounding?region=seasia&TYPE=TEXT%3ALIST&YEAR={year}&MONTH={month:02d}&FROM=0100&TO=3112&STNM=96749" 
+        driver.get(url)
+        
+        tag_body = driver.find_elements(By.TAG_NAME, "body")
+        # Menyimpan data ke dalam file
+        file_path = f'download/data_{year}_{month:02d}.txt'
+        with open(file_path, 'w') as file:
+            for body in tag_body:
+                file.write(body.text)
+        
+        if driver.find_elements(By.TAG_NAME, "pre")[0].text == "Invalid TIME parameter." :
+            # untuk bulan dengan jumlah tanggal 30
+            url = f"https://weather.uwyo.edu/cgi-bin/sounding?region=seasia&TYPE=TEXT%3ALIST&YEAR={year}&MONTH={month:02d}&FROM=0100&TO=3012&STNM=96749"
+            driver.get(url)
+
+            tag_body = driver.find_elements(By.TAG_NAME, "body")
+            # Menyimpan data ke dalam file
+            file_path = f'download/data_{year}_{month:02d}.txt'
+            with open(file_path, 'w') as file:
+                for body in tag_body:
+                    file.write(body.text)
+            
+            if driver.find_elements(By.TAG_NAME, "pre")[0].text == "Invalid TIME parameter.":
+                # untuk bulan dengan jumlah tanggal 29
+                url = f"https://weather.uwyo.edu/cgi-bin/sounding?region=seasia&TYPE=TEXT%3ALIST&YEAR={year}&MONTH={month:02d}&FROM=0100&TO=2912&STNM=96749"
+                driver.get(url)
+
+                tag_body = driver.find_elements(By.TAG_NAME, "body")
+                # Menyimpan data ke dalam file
+                file_path = f'download/data_{year}_{month:02d}.txt'
+                with open(file_path, 'w') as file:
+                    for body in tag_body:
+                        file.write(body.text)           
+
+                if driver.find_elements(By.TAG_NAME, "pre")[0].text == "Invalid TIME parameter.":
+                    # untuk bulan dengan jumlah tanggal 28
+                    url = f"https://weather.uwyo.edu/cgi-bin/sounding?region=seasia&TYPE=TEXT%3ALIST&YEAR={year}&MONTH={month:02d}&FROM=0100&TO=2812&STNM=96749"
+                    driver.get(url)
+
+                    tag_body = driver.find_elements(By.TAG_NAME, "body")
+                    # Menyimpan data ke dalam file
+                    file_path = f'download/data_{year}_{month:02d}.txt'
+                    with open(file_path, 'w') as file:
+                        for body in tag_body:
+                            file.write(body.text)
+
+                else:
+                    pass
+            else:
+                pass
+        else:
+            pass
+    except:
+        pass
+
+time.sleep(1)
+driver.close()
+
+# bagian menyusun dataframenya dari kumpulan download file txtnya
+# Nama folder output
+output_folder = 'output'
+
+os.makedirs(output_folder, exist_ok=True)
+
+# bisa diatasi dengan mengganti nama file saat download kalau mau simpel untuk sortednya
+file_names = os.listdir("download/")
+sorted_file_names = sorted(file_names, key=lambda x: (int(x.split('_')[2][:2]), int(x.split('_')[1])))
+with open(os.path.join(output_folder, 'coba.txt'), 'w') as outfile:
+    for fname in sorted_file_names:
+        with open(os.path.join("download", fname)) as infile:
+            for line in infile:
+                outfile.write(line)
+
+with open(os.path.join(output_folder, 'coba.txt'), 'r') as file:
+    text = file.read()
+
+lines = text.strip().split('\n')
+data = []
+
+for line in lines:
+    if "Station information and sounding indices" in line:
+        station_data = {}
+    if ":" in line:
+        key, value = line.split(':')
+        station_data[key.strip()] = value.strip()
+        # Jika sudah mencapai SWEAT index, tambahkan data stasiun ke list dan reset dict
+        if "SWEAT index" in key:
+            data.append(station_data)
+# Buat DataFrame dari list data
+df = pd.DataFrame(data)
+
+# save filenya
+df.to_excel('output/hasil.xlsx')
